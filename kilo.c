b839b714ceee0f00e07510d25320c7ef1766ba9e
Ryan Trinh <rqtrinh@cpp.edu>
2021-12-10 07:23:36 -0800

    Fixed small formatting error

--- kilo.c
+++ kilo.c
@@ -691,7 +691,7 @@ struct abuf {
 
 #define ABUF_INIT {NULL, 0}
 
-void abAppend( struct abuf *ab, const char *s, int len) {
+void abAppend(struct abuf *ab, const char *s, int len) {
     char *new = realloc(ab->b, ab->len + len);
 
     if (new == NULL) return;
